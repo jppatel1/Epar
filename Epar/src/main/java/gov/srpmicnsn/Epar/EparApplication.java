f5715d0ec0d3a126979251e1955295eeb55c1b58
jppatel1 <jaypatel10673@gmail.com>
2020-05-19 14:16:04 -0700

    Add files via upload

---
+++ Epar/src/main/java/gov/srpmicnsn/Epar/EparApplication.java
@@ -0,0 +1,13 @@
+package gov.srpmicnsn.Epar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EparApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EparApplication.class, args);
+	}
+
+}
---
+++ Epar/src/test/java/gov/srpmicnsn/Epar/EparApplicationTests.java
@@ -0,0 +1,13 @@
+package gov.srpmicnsn.Epar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EparApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
